43e72acb531ca093b3e9677591bdcabd093d5d15
kierenpinto <blizzard-123@live.com>
2017-09-23 04:12:42 +0000

    Convert to PHP

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$txt = 'Hello Everyone!'
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Test PHP File
+        </title>
+    </head>
+    <body>
+        This is a text test: </br>
+        <?php echo $txt ?>
+    </body>
+</html>
