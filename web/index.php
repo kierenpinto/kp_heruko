ef4c9dda24e564b04be066933a48c54212872fe2
Kieren Pinto <kieren.pinto@hotmail.com>
2017-10-08 00:41:51 +1100

    heroku test

--- web/index.php
+++ web/index.php
@@ -14,5 +14,18 @@ $txt = 'Hello Everyone!'
         <?php echo $txt ?>
 <br/>
 The directory is: <?php echo __DIR__; ?>
+</br>
+<?php
+$query = $_SERVER['PHP_SELF'];
+$path = pathinfo( $query );
+$what_you_want = $path['dirname'];
+echo $what_you_want;
+?>
+
+<br/>
+<?php
+$filename = basename($_SERVER['REQUEST_URI']);
+echo $filename;
+?>
     </body>
 </html>
